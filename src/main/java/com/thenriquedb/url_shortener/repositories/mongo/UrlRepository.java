d929ecea6a916038ea9db0f751db947a91f9c44b
thenriquedb <thenrique2012@gmail.com>
2025-05-21 11:00:03 -0300

    refactor: remove unused import from UrlRepository

--- src/main/java/com/thenriquedb/url_shortener/repositories/mongo/UrlRepository.java
+++ src/main/java/com/thenriquedb/url_shortener/repositories/mongo/UrlRepository.java
@@ -1,9 +1,7 @@
-package com.thenriquedb.url_shortener.repositories;
+package com.thenriquedb.url_shortener.repositories.mongo;
 
 import com.thenriquedb.url_shortener.schemas.UrlSchema;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
 
 public interface UrlRepository extends MongoRepository<UrlSchema, String> {
     UrlSchema findByOriginalUrl(String originalUrl);
