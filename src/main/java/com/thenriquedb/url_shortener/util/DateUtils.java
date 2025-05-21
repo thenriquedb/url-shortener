d7e3cfe5d44db5af9908f712dacbfa2d58d24f52
thenriquedb <thenrique2012@gmail.com>
2025-05-21 12:00:19 -0300

    feat: add DateUtils class for calculating time differences in seconds

---
+++ src/main/java/com/thenriquedb/url_shortener/util/DateUtils.java
@@ -0,0 +1,13 @@
+package com.thenriquedb.url_shortener.util;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class DateUtils {
+    public static long calculateDifferenceInSeconds(LocalDateTime expireAt) {
+        return ChronoUnit.SECONDS.between(
+                LocalDateTime.now(),
+                expireAt != null ? expireAt : LocalDateTime.now().plusDays(1)
+        );
+    }
+}
