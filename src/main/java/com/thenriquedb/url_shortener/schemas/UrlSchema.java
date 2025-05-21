f8a025e8a80265ff5f663bfbcf6262d054d16e67
thenriquedb <thenrique2012@gmail.com>
2025-05-21 10:59:04 -0300

    feat: update UrlSchema to support Redis with Serializable implementation

--- src/main/java/com/thenriquedb/url_shortener/schemas/UrlSchema.java
+++ src/main/java/com/thenriquedb/url_shortener/schemas/UrlSchema.java
@@ -7,7 +7,10 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Document(collection = "url")
@@ -15,7 +18,11 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UrlSchema {
+@RedisHash("url")
+public class UrlSchema implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @MongoId
     String id;
     String originalUrl;
