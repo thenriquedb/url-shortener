5437476aeeea71af3103e6994eb6ada94aa2fe93
thenriquedb <thenrique2012@gmail.com>
2025-07-24 16:10:33 -0300

    refactor: move instance public IP output to outputs.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,3 @@ resource "aws_instance" "url_shortener_ec2" {
   }
 }
 
-output "instance_public_ip" {
-  value = aws_instance.url_shortener_ec2.public_ip
-  description = "Public IP of the EC2 instance"
-}
